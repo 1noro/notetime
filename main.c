b12aea7ea20d0304cb8645454a0c9ac3465698e5
1noro <ppuubblliicc@protonmail.com>
2021-02-22 22:14:01 +0100

    codigo comentado y preparado para parsear los argumentos

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 #include <time.h>
 #include <string.h>
 
-const int HEADER_LEN = 21;
-const int NOTE_MAXLEN = 280; // tweet
+const int HEADER_LEN = 21; // tamaño exacto del hedaer
+const int NOTE_MAXLEN = 280; // caracteres máximos de una nota (un tweet)
 
-void get_datetime_header(time_t now, char *datetime_str) {
+void get_datetime_header(time_t now, char* datetime_str) {
     // variables to store date and time components
     int hours, minutes, seconds, day, month, year;
 
     // localtime converts a time_t value to calendar time and
     // returns a pointer to a tm structure with its members
     // filled with the corresponding values
-    struct tm *local = localtime(&now);
+    struct tm* local = localtime(&now);
 
     hours = local->tm_hour;          // get hours since midnight (0-23)
     minutes = local->tm_min;         // get minutes passed after the hour (0-59)
@@ -32,13 +32,26 @@ void get_datetime_header(time_t now, char *datetime_str) {
 }
 
 // change \n to \0
-void n_to_0(int str_len, char *str) {
+void n_to_0(int str_len, char* str) {
     for (int i = 0; i < str_len; i++) {
         if (str[i] == '\n') str[i] = '\0';
     }
 }
 
-int main(int argc, char const *argv[]) {
+void print_argv(int argc, char* argv[]) {
+    printf("argc: %d\n", argc);
+    printf("argv: [");
+    for (size_t i = 0; i < argc; i++) {
+        printf("'%s'", argv[i]);
+        if (i != argc - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+int main(int argc, char* argv[]) {
+    // print_argv(argc, argv);
     
     // print header
     time_t now; // time_t is arithmetic time type
@@ -50,7 +63,7 @@ int main(int argc, char const *argv[]) {
 
     // read note
     // (esto aquí no tien sentido, porque solo se hace un vez, pero luego, cuando se lea del archivo, hacerlo así será lo mejor)
-    char *note;
+    char* note;
     char note_aux[NOTE_MAXLEN];
     printf("> ");
     fgets(note_aux, NOTE_MAXLEN, stdin);
